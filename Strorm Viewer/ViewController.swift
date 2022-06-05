fd80ab95abd1f1ffd22acfd8658035411471abca
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-05 12:10:25 +0300

    Initial Commit

---
+++ Strorm Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strorm Viewer
+//
+//  Created by Nikita  on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
