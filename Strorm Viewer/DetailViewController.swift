eed9956fa0905695b2f887c01b68fbec8199ff30
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-05 15:41:03 +0300

    functionality for presenting images

--- Strorm Viewer/DetailViewController.swift
+++ Strorm Viewer/DetailViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let image = selectedImage{
+            imageView.image = UIImage(named: image)
+        }
+        
 
        
     }
