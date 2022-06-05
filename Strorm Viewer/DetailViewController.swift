d933e769bae85068cb610ea3a7c47118c7f243ab
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-05 13:59:31 +0300

    created secondVC for showing storm images

---
+++ Strorm Viewer/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Strorm Viewer
+//
+//  Created by Nikita  on 6/5/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+  
+}
