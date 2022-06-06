4f21b2da505ee1513ab523aebbea408794fe9ef4
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-06 17:30:40 +0300

    added uiactivity controller

--- Strorm Viewer/DetailViewController.swift
+++ Strorm Viewer/DetailViewController.swift
@@ -20,7 +20,10 @@ class DetailViewController: UIViewController {
         }
         
         title = "Picture \(Int(images.firstIndex(of: selectedImage ?? "")!) + 1) of \(images.count)"
+        
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
     }
     
@@ -33,6 +36,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+    @objc func shareTapped(){
+        guard let image = (imageView.image?.jpegData(compressionQuality: 1)) else{
+            print("No image found ")
+            return
+        }
+        let dc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        dc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(dc, animated: true, completion: nil)
     }
     
 
